2c9ce33cf51838340a526729fd6c31a3ce488b27
Toluwanimi Kuti <toluwanimi.kuti@pau.edu.ng>
2026-08-30 16:17:20 +0100

    updated practice_1.rs

--- week-1/practice_1.rs
+++ week-1/practice_1.rs
@@ -1,4 +1,12 @@
-fn mani(){
-	println!("Welcom to COS101");
+fn main() {
+   println!("Welcome to COS101");
 
-//Random code for practicing goes here soon...
+   //Random beginner code for practicing goes here soon...
+
+   //Use of the let variable
+   let name = "Arizona";
+   let age = "99";
+   let providence = "Somewhere sha";
+
+   println!("My name is {} and I am about to turn {}.\nIm from {}.", name, age, providence);
+}
