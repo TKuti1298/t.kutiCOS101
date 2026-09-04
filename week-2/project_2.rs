7bf9289a5358edabe265de01607fe38e6a6e07d8
Toluwanimi Kuti <toluwanimi.kuti@pau.edu.ng>
2026-09-04 13:49:20 +0100

    updated and completed week 2

--- week-2/project_2.rs
+++ week-2/project_2.rs
@@ -1,14 +1,14 @@
 fn main(){
-	let toshiba = 450_000*2; //900000
-	let mac = 1_500_000*1; //1500000
-	let hp = 750_000*3; //2250000
-	let dell = 2_850_000*3; //8550000
-	let acer = 250_000*1; //250000
-	let tq = 10.0;
+	let toshiba:f64 = 900_000.0; //450_000*2
+	let mac:f64 = 1_500_000.0; //1_500_000*1
+	let hp:f64 = 2_250_000.0; //750_000*3
+	let dell:f64 = 8_550_000.0; //2_850_000*3
+	let acer:f64 = 250_000.0; //250_000*1
+	let tq:f64 = 10.0;
 //these items values are actually their amount multiplied by their quantity.
 
 let s = toshiba + mac + hp + dell + acer; 
-let a = s as f64/tq;
+let a = s/tq;
 println!("The sum of the sales of the items from the sales report would be {}.\nWhile the average sales of items from the sales report would be {}.",s,a)
 
 }
